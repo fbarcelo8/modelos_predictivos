cbe42de43b77869038d6b2df812aba971ed9ab64
Fernando Barceló <144925937+fbarcelo8@users.noreply.github.com>
2025-01-20 13:22:24 +0100

    Update app2.py

--- app2.py
+++ app2.py
@@ -717,7 +717,7 @@ def main():
     # Crear columnas para centrar la imagen
     col1, col2, col3 = st.columns([1, 4, 1])  # Ajusta las proporciones para centrar la imagen
     with col2:
-        st.image("C:/Users/barce/Desktop/logo_butler.png", width=400)
+        st.image("images/logo_butler1.png", width=450)
 
     # Mostrar el título centrado y más grande
     st.markdown(
