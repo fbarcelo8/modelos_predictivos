830e2c4fb5912e80af48d6300afbeaecb5367581
Fernando Barceló <144925937+fbarcelo8@users.noreply.github.com>
2025-01-22 11:12:01 +0100

    Update step6.py

--- modules/step6.py
+++ modules/step6.py
@@ -43,7 +43,7 @@ def step_6():
     if target_type == "Numérica":
         st.markdown(
             "**Regresión Lineal:** Se ha dividido el conjunto de datos en {}% entrenamiento y {}% prueba."
-            .format(int(train_size * 100), int(test_size * 100))
+            .format(round(train_size * 100), round(test_size * 100))
         )
         model = Pipeline([
             ("preprocessor", preprocessor),
