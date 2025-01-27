e231264dae6334f207584633f03d796f1274b459
Fernando Barceló <144925937+fbarcelo8@users.noreply.github.com>
2025-01-27 11:57:24 +0100

    Update step6.py

--- modules/step6.py
+++ modules/step6.py
@@ -7,7 +7,7 @@ import statsmodels.api as sm
 from sklearn.compose import ColumnTransformer 
 from sklearn.preprocessing import StandardScaler, OneHotEncoder 
 from sklearn.pipeline import Pipeline 
-from modules.utils import reset_steps, seleccion_forward_bic, update_predictors_lists
+from modules.utils import reset_steps, seleccion_forward_bic, update_predictor_lists
 
 def step_6():
     if not st.session_state.get('step_6_enabled', False):
