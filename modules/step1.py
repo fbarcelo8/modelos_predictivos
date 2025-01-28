b5fc4ea0f6c4825e1e2ef00d0730c1716ddbf552
Fernando Barceló <144925937+fbarcelo8@users.noreply.github.com>
2025-01-28 12:13:28 +0100

    Update step1.py

--- modules/step1.py
+++ modules/step1.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 from modules.preprocessing import preprocess_dataset
 from modules.utils import reset_steps
+
 def step_1():
     st.header("Paso 1: Subida de Archivo")
     uploaded_file = st.file_uploader("Sube tu archivo CSV o Excel", type=["csv", "xlsx", "xls"])
